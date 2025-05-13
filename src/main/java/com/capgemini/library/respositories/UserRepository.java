14c2105489cc90380213026521c955af9cb2e0e0
Aryan Parmanand <aparmanand22@gmail.com>
2025-05-13 22:01:20 +0530

    Trying Hard

--- src/main/java/com/capgemini/library/respositories/UserRepository.java
+++ src/main/java/com/capgemini/library/respositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.capgemini.library.respositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capgemini.library.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
